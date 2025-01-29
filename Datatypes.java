1f560135193a4eab6d25fe99f346950918079fae
ArchanaIndla <indlaarchana4751@gmail.com>
2025-01-29 08:22:45 +0530

    Add files via upload

---
+++ Datatypes.java
@@ -0,0 +1,16 @@
+public class Datatypes{
+        public static void main(String[] args) {
+            int a=4;
+            char b='A';
+            String c="Archana";
+            float d=11.2f;
+            double e=10.3;
+            Boolean f=true;
+            System.out.println(a);
+            System.out.println(b);
+            System.out.println(c);
+            System.out.println(d);
+            System.out.println(e);
+            System.out.println(f);
+        }
+    }
